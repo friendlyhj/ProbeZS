c46d6df400a4ac0b715f09e8587e5fe68d4be28d
youyihj <youyi580@qq.com>
2023-05-13 13:46:52 +0800

    remove debug code

--- src/main/java/youyihj/probezs/core/ProbeZSClassTransformer.java
+++ src/main/java/youyihj/probezs/core/ProbeZSClassTransformer.java
@@ -1,14 +1,10 @@
 package youyihj.probezs.core;
 
 import net.minecraft.launchwrapper.IClassTransformer;
-import org.apache.commons.io.FileUtils;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.Opcodes;
 import youyihj.probezs.core.asm.ASMCraftTweakerAPI;
-
-import java.io.File;
-import java.io.IOException;
 
 /**
  * @author youyihj
@@ -21,13 +17,7 @@ public class ProbeZSClassTransformer implements IClassTransformer {
             ClassWriter classWriter = new ClassWriter(0);
             ASMCraftTweakerAPI asm = new ASMCraftTweakerAPI(Opcodes.ASM5, classWriter);
             new ClassReader(basicClass).accept(asm, 0);
-            byte[] result = classWriter.toByteArray();
-            try {
-                FileUtils.writeByteArrayToFile(new File("CraftTweakerAPI.class"), result);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            return result;
+            return classWriter.toByteArray();
         }
         return basicClass;
     }
