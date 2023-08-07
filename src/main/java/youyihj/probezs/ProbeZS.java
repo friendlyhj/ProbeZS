56cd9e210ca902d344c2497b8dff1992a8a78d5c
youyihj <youyi580@qq.com>
2023-08-07 19:13:26 +0800

    v1.9.4

--- src/main/java/youyihj/probezs/ProbeZS.java
+++ src/main/java/youyihj/probezs/ProbeZS.java
@@ -51,7 +51,7 @@ import java.util.stream.Collectors;
 @Mod(modid = ProbeZS.MODID, name = ProbeZS.NAME, version = ProbeZS.VERSION, dependencies = ProbeZS.DEPENDENCIES)
 public class ProbeZS {
     public static final String MODID = "probezs";
-    public static final String VERSION = "1.9.3";
+    public static final String VERSION = "1.9.4";
     public static final String NAME = "ProbeZS";
     public static final String DEPENDENCIES = "required-after:crafttweaker;";
     public static Logger logger;
