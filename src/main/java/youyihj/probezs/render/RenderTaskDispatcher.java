298dc57dcfb1c7bf7f36b179242fa7103ddb9332
youyihj <youyi580@qq.com>
2023-10-02 18:39:49 +0800

    directly run future task

--- src/main/java/youyihj/probezs/render/RenderTaskDispatcher.java
+++ src/main/java/youyihj/probezs/render/RenderTaskDispatcher.java
@@ -1,10 +1,8 @@
 package youyihj.probezs.render;
 
-import net.minecraft.util.Util;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
-import youyihj.probezs.ProbeZS;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
@@ -32,7 +30,7 @@ public class RenderTaskDispatcher {
         if (event.phase == TickEvent.Phase.START) {
             synchronized (tasks) {
                 while (!tasks.isEmpty()) {
-                    Util.runTask(tasks.poll(), ProbeZS.logger);
+                    tasks.poll().run();
                 }
             }
         }
