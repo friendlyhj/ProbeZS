71af71080134d950dad75d334a17bb49b23574a3
youyihj <youyi580@qq.com>
2023-12-14 19:59:14 +0800

    some jvm doesn't support boot classpath, 1.17.9

--- src/main/java/youyihj/probezs/ProbeZS.java
+++ src/main/java/youyihj/probezs/ProbeZS.java
@@ -81,7 +81,7 @@ import java.util.stream.Collectors;
 @Mod(modid = ProbeZS.MODID, name = ProbeZS.NAME, version = ProbeZS.VERSION, dependencies = ProbeZS.DEPENDENCIES)
 public class ProbeZS {
     public static final String MODID = "probezs";
-    public static final String VERSION = "1.17.8";
+    public static final String VERSION = "1.17.9";
     public static final String NAME = "ProbeZS";
     public static final String DEPENDENCIES = "required-after:crafttweaker;";
     public static Logger logger;
@@ -335,7 +335,11 @@ public class ProbeZS {
         RuntimeMXBean runtimemxbean = ManagementFactory.getRuntimeMXBean();
         Environment.put("jvmFlags", runtimemxbean.getInputArguments());
         Environment.put("classpath", runtimemxbean.getClassPath());
-        Environment.put("bootClassPath", runtimemxbean.getBootClassPath());
+        try {
+            Environment.put("bootClassPath", runtimemxbean.getBootClassPath());
+        } catch (UnsupportedOperationException e) {
+            Environment.put("bootClassPath", "");
+        }
         Environment.output(generatedPath.resolve("env.json"));
     }
 
