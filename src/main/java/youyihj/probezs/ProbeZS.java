a00969f09636c9e88645a6421e38a91be661d1ba
friendlyhj <youyi580@qq.com>
2026-07-29 18:38:20 +0800

    v1.18.10

--- src/main/java/youyihj/probezs/ProbeZS.java
+++ src/main/java/youyihj/probezs/ProbeZS.java
@@ -100,9 +100,9 @@ import java.util.stream.Collectors;
 @Mod(modid = ProbeZS.MODID, name = ProbeZS.NAME, version = ProbeZS.VERSION, dependencies = ProbeZS.DEPENDENCIES)
 public class ProbeZS {
     public static final String MODID = "probezs";
-    public static final String VERSION = "1.18.9";
+    public static final String VERSION = "1.18.10";
     public static final String NAME = "ProbeZS";
-    public static final String DEPENDENCIES = "required-after:crafttweaker;";
+    public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:mixinbooter@[11.5,)";
     public static Logger logger;
     private static final List<LoadingObject<?>> loadingObjects = new ArrayList<>();
 
